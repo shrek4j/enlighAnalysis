d3cb6aef802efa20a246949822b0db1e7f943343
shrek4j <scxtjwgy2005@163.com>
2017-04-27 17:15:54 +0800

    update scripts

---
+++ untitled/morph/generateMorphemeScore.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+import math
+import time
+import re
+import MySQLdb
+from urllib import urlopen
+import urllib2
+from bs4 import BeautifulSoup
+
+db = MySQLdb.connect('localhost', 'root', 'Password123', 'lexi_db', charset="utf8")
+cursor = db.cursor()
+
+def grab_word_mission():
+    rows = getMorphemes()
+    for row in rows:
+        calculateScore(row)
+        db.commit()
+
+
+def calculateScore(row):
+    totalFreq = 0
+    wfdict = {}
+    wfs = getWordFreqs(row[0])
+    for wf in wfs:
+        wfdict[wf[2]] = wf[1]
+        swfs = getSimilarWordFreqs(wf[0])
+        for swf in swfs:
+            if wfdict.has_key(swf[2]):
+                pass
+            else:
+                wfdict[swf[2]] = swf[1]
+
+    for k in wfdict.keys():
+        totalFreq += wfdict[k]
+
+    totalLogFreq = 0
+    if totalFreq > 0:
+        totalLogFreq = round(math.log(totalFreq),2)
+    print row[0],totalLogFreq,totalFreq
+    updateFreqLevel(row[0],totalLogFreq,totalFreq)
+
+def getMorphemes():
+    sql = "select id,word_root from wiki_word_root"
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    return rows
+
+def getSimilarWordFreqs(wordId):
+    sql = "SELECT ww.id,ww.freq,ww.word FROM wiki_word_rela wwrr LEFT JOIN wiki_word ww ON wwrr.similar_word_id = ww.id WHERE word_id = "+str(wordId)+" AND has_translation = 1"
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    return rows
+
+def getWordFreqs(wordRootId):
+    sql = "SELECT ww.id,ww.freq,ww.word  FROM wiki_word_root_rela wwrr LEFT JOIN wiki_word ww ON wwrr.word_id = ww.id WHERE word_root_id = "+str(wordRootId)+" AND has_translation = 1"
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    return rows
+
+def updateFreqLevel(wordRootId,totalLogFreq,totalFreq):
+    sql = "update wiki_word_root set total_log_freq = " + str(totalLogFreq) + ",total_freq="+ str(totalFreq) +" where id=" + str(wordRootId)
+    cursor.execute(sql)
+
+
+def closeDb(cursor, db):
+    if cursor:
+        cursor.close()
+    if db:
+        db.close()
+
+#do the mission
+if __name__ == '__main__':
+    try:
+        grab_word_mission()
+    finally:
+        closeDb(cursor,db)
